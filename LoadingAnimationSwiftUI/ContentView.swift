2373b1e5f2f667b09c6c4f4ea1bb84dae5d20690
Ashish Tyagi <ashisht@dewsolutions.in>
2020-09-12 20:14:40 +0530

    Initial Commit

---
+++ LoadingAnimationSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoadingAnimationSwiftUI
+//
+//  Created by Ashish Tyagi on 12/09/20.
+//  Copyright © 2020 Ashish Tyagi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
